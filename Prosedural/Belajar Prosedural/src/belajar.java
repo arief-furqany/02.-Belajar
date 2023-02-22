607359173725434a4b04526b3b600f297a83b528
Musixient <m.arief.furqany@gmail.com>
2023-02-23 03:39:02 +0700

    ubah dikit, tambahin variable dan isi variable

--- Prosedural/Belajar Prosedural/src/belajar.java
+++ Prosedural/Belajar Prosedural/src/belajar.java
@@ -4,10 +4,10 @@ public class belajar{
 
         String nama = ("Muhammad arief furqany");
         int nim = 220705043;
-        Boolean
-        Double
-        Float
-        char
+        Boolean hujan = true;
+        Double r = 70.8;
+        Float a = 3.7;
+        char v = ("a");
         
         System.out.println(nama + " " + nim);
         System.out.println();
