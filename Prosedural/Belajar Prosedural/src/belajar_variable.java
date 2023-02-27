cdfcad6a96b40969bf5bffca59fe46d2f31ad243
Musixient <m.arief.furqany@gmail.com>
2023-02-27 23:59:33 +0700

    Nambahin komentar buat mempermudah pemahaman kode

--- Prosedural/Belajar Prosedural/src/belajar_variable.java
+++ Prosedural/Belajar Prosedural/src/belajar_variable.java
@@ -2,15 +2,28 @@ public class belajar_variable{
 
     public static void main(String[] args) {
 
+        //String adalah untuk tipe data huruf, kalimat
         String nama = ("Muhammad arief furqany");
+
+        //int adalah Integer, deklarasi variable untuk tipe data angka bulat
         int nim = 220705043;
+
+        //Boolean adalah deklarasi untuk variable yang memiliki nilai benar atau salah
         Boolean hujan = true;
-        Double r = 70.8;
-        Float a = 3.7;
-        char v = ("a");
+
+        //Double adalah tipe data yang menampung nilai desimal
+        Double a = 70.8;
+
+        //Float adalah tipe data yang menampug nilai desimal dengan luas tampunan lebih banyak daripada Double
+        Float b = 3.7;
+
+        //Char adalah tipe data yang hanya bisa menampung data huruf, dan hanya satu huruf
+        char c = ("a");
         
         System.out.println(nama + " " + nim);
-        System.out.println();
+        System.out.println( "ini adalah tipe data boolean yang bernilai: " + hujan );
+        System.out.println("ini adalah tipedata double dikali dengan Float: " + a * b);
+        System.out.println( "ini adalah tipe data Char: " + c );
         
     }
 
