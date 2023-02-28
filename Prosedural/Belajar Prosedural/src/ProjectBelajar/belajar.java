6765ff146f11f3f376f9e30e1da47c2e3efff69b
Musixient <m.arief.furqany@gmail.com>
2023-02-28 12:47:47 +0700

    Penambahan comment, pembuatan fungsi close untuk menutup Scanner

---
+++ Prosedural/Belajar Prosedural/src/ProjectBelajar/belajar.java
@@ -0,0 +1,5 @@
+package projectBelajar;
+
+public class belajar {
+    
+}
---
+++ Prosedural/Belajar Prosedural/src/loop_while.java
@@ -0,0 +1,15 @@
+public class loop_while {
+
+    public static void main(String[] args) {
+        
+       //Standart Programs untuk while 
+        int Student = 0; // adalah titik memulai
+        while (Student < 10){ //batasan pengulangan adalah 10
+            System.out.println("student"+Student);
+        Student++; // increment(peningkatan)
+        }   
+    }
+}
+
+
+ 
