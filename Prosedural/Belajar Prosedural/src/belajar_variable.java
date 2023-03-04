f1daa3cdc39809ccbd4744d022258ad30741d861
ariekany <m.arief.furqany@gmail.com>
2023-03-04 13:52:50 +0700

    menambahkan tipe variable dan perbaikan code

--- Prosedural/Belajar Prosedural/src/belajar_variable.java
+++ Prosedural/Belajar Prosedural/src/belajar_variable.java
@@ -1,31 +1,32 @@
-public class belajar_variable{
-
+public class belajar_variable {
 
     public static void main(String[] args) {
 
-        //String adalah untuk tipe data huruf, kalimat
+        // String adalah untuk tipe data huruf, kalimat
         String nama = ("Muhammad arief furqany");
 
-        //int adalah Integer, deklarasi variable untuk tipe data angka bulat
+        // int adalah Integer, deklarasi variable untuk tipe data angka bulat
         int nim = 220705043;
 
-        //Boolean adalah deklarasi untuk variable yang memiliki nilai benar atau salah
+        // Boolean adalah deklarasi untuk variable yang memiliki nilai benar atau salah
         Boolean hujan = true;
 
-        //Double adalah tipe data yang menampung nilai desimal
+        // Double adalah tipe data yang menampung nilai desimal
         Double a = 70.8;
 
-        //Float adalah tipe data yang menampug nilai desimal dengan luas tampunan lebih banyak daripada Double
+        // Float adalah tipe data yang menampug nilai desimal dengan luas tampunan lebih
+        // banyak daripada Double
         Float b = 3.7f;
 
-        //Char adalah tipe data yang hanya bisa menampung data huruf, dan hanya satu huruf
+        // Char adalah tipe data yang hanya bisa menampung data huruf, dan hanya satu
+        // huruf
         char c = 'd';
-        
+
         System.out.println(nama + " " + nim);
-        System.out.println( "ini adalah tipe data boolean yang bernilai: " + hujan );
+        System.out.println("ini adalah tipe data boolean yang bernilai: " + hujan);
         System.out.println("ini adalah tipedata double dikali dengan Float: " + a * b);
-        System.out.println( "ini adalah tipe data Char: " + c );
-        
+        System.out.println("ini adalah tipe data Char: " + c);
+
     }
 
 }
