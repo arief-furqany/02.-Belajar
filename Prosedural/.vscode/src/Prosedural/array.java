cb36e0eb8f7a6799404776633d748895b2b111e2
ariekany <m.arief.furqany@gmail.com>
2023-03-11 18:37:42 +0700

    update arrray

---
+++ Prosedural/.vscode/src/Prosedural/array.java
@@ -0,0 +1,55 @@
+public class array {
+    
+    public static void main(String[] args) {
+        System.out.println("Belajar Array");
+
+
+        // [] = "kurung siku"
+        // kurung siku adalah cara mendeklarasikan sebuah array
+        //kurung siku diletakkan setelah tipe data atau nama array
+        //Angka dalam kurung siku merupakan batas dari ukuran array
+
+
+
+        //ada tiga cara membuat array pada java, dan cara berikut adalah array kosong
+        System.out.println("cara pertama");
+        String[] nama;
+
+        System.out.println("cara kedua");
+        String buah[];
+
+        System.out.println("cara ketiga");
+        String[] nomor_urut = new String [5];
+
+
+        //cara mengisi array kosong adalah dengan cara memberikan
+        //nilai dalam kurung siku setelah pemanggilan variable array
+
+        System.out.println("cara kedua");
+        String meme[] = {"masbro", "acumalaka", "nigger"};
+
+        System.out.println("cara pertama");
+        String[] niga = new string[10];
+        
+        //fungsi length digunakan untuk mengetahui jumlah isi dari array
+        //length biasa digunakan pada perulangan
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
--- Prosedural/.vscode/src/Prosedural/loop_for.java
+++ Prosedural/.vscode/src/Prosedural/loop_for.java
@@ -4,6 +4,7 @@ public static void main(String[] args) {
     System.out.println("Ini adalah looping");
 
         for (int i = 0; i < 10; i++) {
+         
             System.out.println("Loop iteration: " + i);
         }
     } 
