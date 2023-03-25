5e0f74f86375c1059991a8c761dee75ad3c6d3c0
Musixient <m.arief.furqany@gmail.com>
2023-03-25 23:12:38 +0700

    Maintenance code operator logika

--- Prosedural/src/BelajarProsedural/OperatorLogika.java
+++ Prosedural/src/BelajarProsedural/OperatorLogika.java
@@ -6,8 +6,20 @@ public class OperatorLogika{
         System.out.println("Operator Logika");
         // System.out.println("adalah operator untuk membuat program logika dengan menggabungkan premis");
 
+        String logika = ("bernilai 10");
+        String Algoritma = ("bernilai 20");
         
+        //operator and (Konjungsi)
+        System.out.println(logika + Algoritma);
 
+        //operator or (Disjungsi)
+        System.out.println();
+
+        //operator Implikasi
+        System.out.println();
+
+        //operator biimplikasi
+        System.out.println();
     }
 
 }
