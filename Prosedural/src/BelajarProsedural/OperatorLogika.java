a129cfe8b076bf50f89886c6210b31edcb8c31f3
Musixient <m.arief.furqany@gmail.com>
2023-03-30 00:39:52 +0700

    pembuatan code pada operator OperatorLogika

--- Prosedural/src/BelajarProsedural/OperatorLogika.java
+++ Prosedural/src/BelajarProsedural/OperatorLogika.java
@@ -2,24 +2,35 @@ package BelajarProsedural;
 
 public class OperatorLogika{
 
+   
     public static void main(String[] args) {
         System.out.println("Operator Logika");
         // System.out.println("adalah operator untuk membuat program logika dengan menggabungkan premis");
 
-        String logika = ("bernilai 10");
-        String Algoritma = ("bernilai 20");
-        
-        //operator and (Konjungsi)
-        System.out.println(logika + Algoritma);
+        int a = 10;
+      int b = 20;
+      int c = 30;
 
-        //operator or (Disjungsi)
-        System.out.println();
+      // Logical AND operator
+      if (a > b && a > c) {
+         System.out.println("a is the largest number.");
+      }
 
-        //operator Implikasi
-        System.out.println();
+      // Logical OR operator
+      if (b > a || c > a) {
+         System.out.println("Either b or c is the largest number.");
+      }
 
-        //operator biimplikasi
-        System.out.println();
+      // Logical NOT operator
+      boolean x = true;
+      boolean y = false;
+
+      System.out.println(!x); // false
+      System.out.println(!y); // true
+    
     }
-
 }
+```
+
+//This program defines three variables `a`, `b`, and `c` and uses both the logical `&&` and `||` operators in `if` statements to check their values. It also demonstrates the use of the logical `!` operator to negate boolean values.
+   
