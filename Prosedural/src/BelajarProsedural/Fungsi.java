200e47759b0fbea5f47772ab79b133a2d23971cb
Musixient <m.arief.furqany@gmail.com>
2023-04-07 20:58:41 +0700

    belajar membuat fungsi pada java

---
+++ Prosedural/src/BelajarProsedural/Fungsi.java
@@ -0,0 +1,19 @@
+package BelajarProsedural;
+import java.util.Scanner;
+
+public class Fungsi {
+    static Scanner input = new Scanner(System.in);
+    public static void main (String[] args){
+
+        
+        int hitung = 23; 
+
+
+    }
+
+    static int belajar(hitung a, hitung b){
+
+
+        return int;
+    }
+}
